e285ff22ea974b0ccfcd6b57d88ea9f3c0112a3d
Ngaukon <josephlucia100@gmail.com>
2024-10-01 18:37:50 +0000

    block scoped

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueOrFalse) {
+  let task = false;  // Use let instead of var
+  let task2 = true;  // Use let instead of var
+
+  if (trueOrFalse) {
+    let task = true;  // This task is scoped to the if block
+    let task2 = false; // This task2 is also scoped to the if block
+  }
+
+  return [task, task2]; // Returns the outer scoped variables
+}
